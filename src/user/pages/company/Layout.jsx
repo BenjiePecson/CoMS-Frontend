7dbd3b094d14da60694dbe7d264ea0ebd99f801f
BenjiePecson <161810296+BenjiePecson@users.noreply.github.com>
2024-04-23 09:11:55 +0800

    Added: Boardmeetings Navigations in Company page

--- src/user/pages/company/Layout.jsx
+++ src/user/pages/company/Layout.jsx
@@ -53,6 +53,18 @@ const Layout = () => {
     </svg>
   );
 
+  const meetingIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="w-6 h-6"
+    >
+      <path d="M4.913 2.658c2.075-.27 4.19-.408 6.337-.408 2.147 0 4.262.139 6.337.408 1.922.25 3.291 1.861 3.405 3.727a4.403 4.403 0 0 0-1.032-.211 50.89 50.89 0 0 0-8.42 0c-2.358.196-4.04 2.19-4.04 4.434v4.286a4.47 4.47 0 0 0 2.433 3.984L7.28 21.53A.75.75 0 0 1 6 21v-4.03a48.527 48.527 0 0 1-1.087-.128C2.905 16.58 1.5 14.833 1.5 12.862V6.638c0-1.97 1.405-3.718 3.413-3.979Z" />
+      <path d="M15.75 7.5c-1.376 0-2.739.057-4.086.169C10.124 7.797 9 9.103 9 10.609v4.285c0 1.507 1.128 2.814 2.67 2.94 1.243.102 2.5.157 3.768.165l2.782 2.781a.75.75 0 0 0 1.28-.53v-2.39l.33-.026c1.542-.125 2.67-1.433 2.67-2.94v-4.286c0-1.505-1.125-2.811-2.664-2.94A49.392 49.392 0 0 0 15.75 7.5Z" />
+    </svg>
+  );
+
   const lightMode = (
     <>
       <svg
@@ -204,7 +216,7 @@ const Layout = () => {
                 </svg>
               </div> */}
               <div className="px-4">
-                <Link
+                {/* <Link
                   to={`/company/${companyId}/`}
                   onClick={() => {
                     setActive("dashboard");
@@ -229,6 +241,164 @@ const Layout = () => {
                     icon={trackerIcon}
                   />
                 </Link>
+
+                <NavBar
+                  isActive={false}
+                  text="Board Meetings"
+                  icon={trackerIcon}
+                /> */}
+
+                <div>
+                  <ul className="menu w-full rounded-box text-white">
+                    <Link
+                      className={`flex flex-row hover:bg-[#667A8A] h-[45px] rounded-[3px] items-center my-1 ${
+                        active === "dashboard" ? "bg-[#667A8A]" : ""
+                      } ${
+                        active === "dashboard"
+                          ? "poppins-semibold"
+                          : "poppins-regular"
+                      } text-white`}
+                      to={`/company/${companyId}/`}
+                      onClick={() => {
+                        setActive("dashboard");
+                      }}
+                    >
+                      <div className="flex flex-row px-4 items-center">
+                        <div className="pr-4">{dashboardIcon}</div>
+                        <div className="text-white">Dashboard</div>
+                      </div>
+                    </Link>
+
+                    <Link
+                      className={`flex flex-row hover:bg-[#667A8A] h-[45px] rounded-[3px] items-center my-1 ${
+                        active === "gis-tracker" ? "bg-[#667A8A]" : ""
+                      } ${
+                        active === "gis-tracker"
+                          ? "poppins-semibold"
+                          : "poppins-regular"
+                      } text-white`}
+                      to={`/company/${companyId}/gis-tracker`}
+                      onClick={() => {
+                        setActive("gis-tracker");
+                      }}
+                    >
+                      <div className="flex flex-row px-4 items-center">
+                        <div className="pr-4">{trackerIcon}</div>
+                        <h1 className="text-white">GIS Tracker</h1>
+                      </div>
+                    </Link>
+                    <li>
+                      <details open>
+                        <summary className="h-[45px] my-1 flex flex-row justify-between items-center">
+                          <div className="flex flex-row items-center gap-4">
+                            {meetingIcon} Board Meetings
+                          </div>
+                        </summary>
+                        <ul>
+                          <Link
+                            className={`flex flex-row hover:bg-[#667A8A] h-[45px] rounded-[3px] items-center my-1 ${
+                              active === "notice-of-meeting"
+                                ? "bg-[#667A8A]"
+                                : ""
+                            } ${
+                              active === "notice-of-meeting"
+                                ? "poppins-semibold"
+                                : "poppins-regular"
+                            } text-white`}
+                            to={`/company/${companyId}/notice-of-meeting`}
+                            onClick={() => {
+                              setActive("notice-of-meeting");
+                            }}
+                          >
+                            <div className="flex flex-row px-4 items-center">
+                              <h1 className="text-white">Notice of Meeting</h1>
+                            </div>
+                          </Link>
+                          <Link
+                            className={`flex flex-row hover:bg-[#667A8A] h-[45px] rounded-[3px] items-center my-1 ${
+                              active === "minutes-of-meeting"
+                                ? "bg-[#667A8A]"
+                                : ""
+                            } ${
+                              active === "minutes-of-meeting"
+                                ? "poppins-semibold"
+                                : "poppins-regular"
+                            } text-white`}
+                            to={`/company/${companyId}/minutes-of-meeting`}
+                            onClick={() => {
+                              setActive("minutes-of-meeting");
+                            }}
+                          >
+                            <div className="flex flex-row px-4 items-center">
+                              <h1 className="text-white">Minutes of Meeting</h1>
+                            </div>
+                          </Link>
+                          <Link
+                            className={`flex flex-row hover:bg-[#667A8A] h-[45px] rounded-[3px] items-center my-1 ${
+                              active === "board-resolution"
+                                ? "bg-[#667A8A]"
+                                : ""
+                            } ${
+                              active === "board-resolution"
+                                ? "poppins-semibold"
+                                : "poppins-regular"
+                            } text-white`}
+                            to={`/company/${companyId}/board-resolution`}
+                            onClick={() => {
+                              setActive("board-resolution");
+                            }}
+                          >
+                            <div className="flex flex-row px-4 items-center">
+                              <h1 className="text-white">Board Resolutions</h1>
+                            </div>
+                          </Link>
+                          <Link
+                            className={`flex flex-row hover:bg-[#667A8A] h-[45px] rounded-[3px] items-center my-1 ${
+                              active === "secretary-certificate"
+                                ? "bg-[#667A8A]"
+                                : ""
+                            } ${
+                              active === "secretary-certificate"
+                                ? "poppins-semibold"
+                                : "poppins-regular"
+                            } text-white`}
+                            to={`/company/${companyId}/secretary-certificate`}
+                            onClick={() => {
+                              setActive("secretary-certificate");
+                            }}
+                          >
+                            <div className="flex flex-row px-4 items-center">
+                              <h1 className="text-white">
+                                Secretary Certificate
+                              </h1>
+                            </div>
+                          </Link>
+                          <Link
+                            className={`flex flex-row hover:bg-[#667A8A] h-[45px] rounded-[3px] items-center my-1 ${
+                              active === "treasurer-certificate"
+                                ? "bg-[#667A8A]"
+                                : ""
+                            } ${
+                              active === "treasurer-certificate"
+                                ? "poppins-semibold"
+                                : "poppins-regular"
+                            } text-white`}
+                            to={`/company/${companyId}/treasurer-certificate`}
+                            onClick={() => {
+                              setActive("treasurer-certificate");
+                            }}
+                          >
+                            <div className="flex flex-row px-4 items-center">
+                              <h1 className="text-white">
+                                Treasurer Certificate
+                              </h1>
+                            </div>
+                          </Link>
+                        </ul>
+                      </details>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
             <div className="px-4">
@@ -393,7 +563,7 @@ const Layout = () => {
   useEffect(() => {
     dispatch(fetchCompany(companyId));
   }, []);
-  
+
   useEffect(() => {
     setActive(
       window.location.pathname.split("/")[3] === "" ||

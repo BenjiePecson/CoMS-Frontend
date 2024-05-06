9390939886e6a318acbc981ceaeddeace400c7eb
BenjiePecson <161810296+BenjiePecson@users.noreply.github.com>
2024-05-06 15:45:12 +0800

    Add update

--- src/user/pages/Login.jsx
+++ src/user/pages/Login.jsx
@@ -1,25 +1,28 @@
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-
+  
+  const user = useSelector((state) => state.user.user);
   const navigate = useNavigate();
   const handleLogin = async (e) => {
     window.open(
-      `${import.meta.env.VITE_VERCEL_SERVER_URL}/auth/google`,
-      "_self",
+      `${import.meta.env.VITE_VERCEL_LOCALHOST_SERVER_URL}/auth/google`,
+      "_self"
     );
   };
 
   useEffect(() => {
     const token = localStorage.getItem("accessToken");
+    console.log(token);
     if (token != null && token != undefined) {
       navigate("/company");
     }
-  });
+  }, [user]);
 
   return (
-    <>
+    <>  
       <div className="flex flex-row w-full min-h-screen justify-center min-w-screen">
         <div className="w-full md:w-2/5 px-[50px] mt-64">
           <div className="text-center text-color-1">

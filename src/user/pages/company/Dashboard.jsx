784e15b4fe8f8c33c6c3813bece6f090947d7eb7
angelo-artiaga <angelo.artiaga@gmail.com>
2024-05-21 14:43:36 +0800

    update dashboard
    add accordion with table for eact files and table for directors/officers

--- src/user/pages/company/Dashboard.jsx
+++ src/user/pages/company/Dashboard.jsx
@@ -1,37 +1,19 @@
-import React, { useEffect } from "react";
-import Header from "../../components/Header";
-import { Link, useParams } from "react-router-dom";
+import React, { useRef } from "react";
+import FinalDocuments from "./DashboardComponents/FinalDocuments";
+import DirectorsTable from "./DashboardComponents/DirectorsTable";
+
+const records = [];
 
 const Dashboard = () => {
-  const { companyId } = useParams();
-
   return (
-    <div>
-      <div className="flex flex-row w-full justify-between items-center">
+    <>
+      <div className="flex flex-col w-full justify-between items-left">
         <div className="poppins-bold text-color-2 text-[24px]">Dashboard</div>
       </div>
 
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 mt-5">
-        {/* <Link to={`/company/${companyId}/gis-tracker/create`}>
-          <div className="card w-full aspect-square bg-base-100 shadow-xl cursor-pointer">
-            <div className="flex flex-col items-center justify-center my-auto py-5 gap-2 w-full">
-              <h1 className="poppins-semibold text-[20px]">Draft GIS</h1>
-              <svg
-                viewBox="0 0 33 34"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-10 h-7"
-              >
-                <path
-                  d="M32.888 21.12H20.792V33.536H12.344V21.12H0.248V13.312H12.344V0.896H20.792V13.312H32.888V21.12Z"
-                  fill="#1F384C"
-                />
-              </svg>
-            </div>
-          </div>
-        </Link> */}
-      </div>
-    </div>
+      <FinalDocuments />
+      <DirectorsTable />
+    </>
   );
 };
 

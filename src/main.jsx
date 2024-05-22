d80eadc84c794e8f61c59c6f588c0cca95a5eb1e
BenjiePecson <161810296+BenjiePecson@users.noreply.github.com>
2024-05-22 14:28:29 +0800

    Revert "Dev"

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { Provider } from "react-redux";
-import { store } from "./user/store/store.js";
-import { GoogleOAuthProvider } from "@react-oauth/google";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <GoogleOAuthProvider
-        clientId={import.meta.env.VITE_VERCEL_GOOGLE_CLIENT_ID}
-      >
-        <App />
-      </GoogleOAuthProvider>
-    </Provider>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
